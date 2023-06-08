72ad476326564048fcd344872473ca12f86f3e27
CarrotHandsome <akekajoo@gmail.com>
2023-06-08 00:13:02 -0700

    resolve with linux

--- Pages/Account/Register.cshtml.cs
+++ Pages/Account/Register.cshtml.cs
@@ -120,16 +120,14 @@ namespace RecipeAZ.Pages.Account
         {
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-            if (ModelState.IsValid)
-            {
+            if (ModelState.IsValid) {
                 var user = CreateUser();
 
                 await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
-                if (result.Succeeded)
-                {
+                if (result.Succeeded) {
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
@@ -143,21 +141,17 @@ namespace RecipeAZ.Pages.Account
 
                     //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
                     //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
-                    await _mailService.Send(Input.Email, "Confirm your email",
+                    await RecipeAZ.Services.MailService.Send(Input.Email, "Confirm your Recipeaz account",
                         $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
+                    if (_userManager.Options.SignIn.RequireConfirmedAccount) {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
-                    }
-                    else
-                    {
+                    } else {
                         await _signInManager.SignInAsync(user, isPersistent: false);
                         return LocalRedirect(returnUrl);
                     }
                 }
-                foreach (var error in result.Errors)
-                {
+                foreach (var error in result.Errors) {
                     ModelState.AddModelError(string.Empty, error.Description);
                 }
             }
--- Services/MailService.cs
+++ Services/MailService.cs
@@ -1,62 +1,61 @@
-﻿using Google.Apis.Gmail.v1;
-using Google.Apis.Services;
-using Google.Apis.Auth.OAuth2;
-using MimeKit;
-using Google.Apis.Util.Store;
-using Google.Apis.Gmail.v1.Data;
-using Google.Apis.Auth.OAuth2.Flows;
-using Google.Apis.Auth.OAuth2.Responses;
+﻿using Amazon;
+using System;
+using System.Collections.Generic;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
 
 namespace RecipeAZ.Services {
     public class MailService {
-        private static string[] Scopes = { GmailService.Scope.GmailSend };
-        private static string ApplicationName = "Recipeaz";
-        public IWebHostEnvironment _env { get; set; }
+        
 
-        public MailService(IWebHostEnvironment env) {
-            _env = env;
-        }
-        public async Task Send(string to, string subject, string bodyText) {
-            var initializer = new BaseClientService.Initializer {
-                HttpClientInitializer = GetCredential(),
-                ApplicationName = ApplicationName,
-            };
+        // The configuration set to use for this email. If you do not want to use a
+        // configuration set, comment out the following property and the
+        // ConfigurationSetName = configSet argument below. 
+        static readonly string configSet = "ConfigSet";
 
-            var service = new GmailService(initializer);
-            
-            var email = new MimeMessage();
-            email.From.Add(new MailboxAddress("Jacob Billings", "carrothands@gmail.com"));
-            email.To.Add(new MailboxAddress(string.Empty, to));
-            email.Subject = subject;
-            email.Body = new TextPart("html") { Text = bodyText };
+        
 
-            var message = new Message {
-                Raw = Base64UrlEncode(email.ToString())
-            };
+        public async static Task Send(string recipientAddress, string subject, string body) {
+            // Replace USWest2 with the AWS Region you're using for Amazon SES.
+            // Acceptable values are EUWest1, USEast1, and USWest2.
+            //var awsAccessKeyId = "AKIAZZF7LNZSBO5YCQ2J";
+            //var awsSecretAccessKey = "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";
+            using (var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.USWest2)) {
+                var sendRequest = new SendEmailRequest {
+                    Source = "carrothands@gmail.com",
+                    Destination = new Destination {
+                        ToAddresses =
+                        new List<string> { recipientAddress }
+                    },
+                    Message = new Message {
+                        Subject = new Content(subject),
+                        Body = new Body {
+                            Html = new Content {
+                                Charset = "UTF-8",
+                                Data = body
+                            },
+                            Text = new Content {
+                                Charset = "UTF-8",
+                                Data = string.Empty
+                            }
+                        }
+                    },
+                    // If you are not using a configuration set, comment
+                    // or remove the following line 
+                    //ConfigurationSetName = configSet
+                };
+                try {
+                    Console.WriteLine("Sending email using Amazon SES...");
+                    var response = await client.SendEmailAsync(sendRequest);
+                    Console.WriteLine("The email was sent successfully.");
+                } catch (Exception ex) {
+                    Console.WriteLine("The email was not sent.");
+                    Console.WriteLine("Error message: " + ex.Message);
 
-            var request = service.Users.Messages.Send(message, "me");
-            await request.ExecuteAsync();
-        }        
-        private UserCredential GetCredential() {
-            UserCredential credential;
-            string credentialPath = "credentials.json";
-            using (var stream = new FileStream(credentialPath, FileMode.Open, FileAccess.Read)) {
-                string credPath = "token.json";
-
-                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.FromStream(stream).Secrets,
-                    Scopes,
-                    "user",
-                    CancellationToken.None,
-                    new FileDataStore(credPath, true)).Result;
+                }
             }
-            return credential;
-        }
 
-        private static string Base64UrlEncode(string input) {
-            var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
-            return Convert.ToBase64String(inputBytes).Replace('+', '-').Replace('/', '_').Replace("=", "");
+            
         }
     }
-
 }
